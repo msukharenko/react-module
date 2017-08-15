7d6fb01c80735df1933da2f3af70591c1e094305
maxim.sukharenko <maxim.sukharenko@cinimex.ru>
2017-08-15 16:56:14 +0300

    доработка шаблона модуля приложения

--- module-template/src/index.js
+++ module-template/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+export default () =>  {
+    return {
+        modulename:'',
+        menulabel:'',
+        permition:{
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+        },
+
+    }
+};
